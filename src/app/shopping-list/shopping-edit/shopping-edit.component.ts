975b1fe13b3c4570899701c2710b38f3930dfcc9
Mark Nelson <mjnelson90@gmail.com>
2018-12-25 20:41:27 +0000

    shopping not working

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,11 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, ElementRef, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnChanges {
+  @ViewChild('nameRef') ServerContentInput: ElementRef
+
+
+    ngOnChanges(){
+      // console.log(`server content input is: ${this.ServerContentInput}`)
+    }
+
+
+  // nameInput(input){
+  //   console.log(`the name is: ${input}`)
+  // }
+
+
+  // amountInput(input){
+  //   console.log(`the amount is: ${input}`)
+  // }
+
+
+
 
   constructor() { }
 
