4666c2b5653539974136e24cc4799d9ee53a6a5f
Mark Nelson <mark.nelson@synchronoss.com>
2019-03-02 16:57:47 +0000

    before I try

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,24 +8,9 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
 
-@Output() recipeSend = new EventEmitter<boolean>();
-@Output() shoppingSend = new EventEmitter<boolean>();
 
 
-  recipeCall(){
-    this.recipeSend.emit(true)
-    this.shoppingSend.emit(false)
-  
-  }
-  
 
-  shoppingCall(){
-    this.shoppingSend.emit(true);
-    this.recipeSend.emit(false);
-
-  }
-
-  constructor() { }
 
   ngOnInit() {
   }
