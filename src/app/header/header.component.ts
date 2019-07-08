08b78ebaff6e6d32fda0edbb79d3f036d9d649e3
mjnel <mjnelson90@gmail.com>
2019-07-08 16:20:14 +0100

    working http

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent   {
+constructor(private dataStorageService: DataStorageService){
+}
 
 
-
-
-
-
-  ngOnInit() {
+  onSaveData(){
+    this.dataStorageService.storeRecipes()
   }
+
+  onFetchData(){
+    this.dataStorageService.fetchRecipes()
+  }
+
+
+
+
 
 }
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import {map} from 'rxjs/operators'
+
+
+@Injectable({providedIn: 'root'})
+
+export class DataStorageService{
+
+constructor(private http:HttpClient, private recipesService: RecipeService){}
+
+storeRecipes(){
+const recipes = this.recipesService.getRecipes(); 
+this.http.put('https://ng-course-project-ab6df.firebaseio.com/recipes.json', recipes)
+    .subscribe(res=>{
+    console.log(res);
+    // another way of doing this would be to put the subscription into the component. This would be handy if the component needed to know about the data
+})
+
+}
+
+
+fetchRecipes(){
+    this.http.get<Recipe[]>('https://ng-course-project-ab6df.firebaseio.com/recipes.json')
+    // Ensuring that the array is either defined or is empty
+    .pipe(map(recipes=>{
+        return recipes.map(recipe =>{
+            return {...recipe, Ingredients:recipe.Ingredients ? recipe.Ingredients : []}            
+        });
+    }))
+    .subscribe(recipes =>{
+    this.recipesService.setRecipes(recipes)
+    })
+
+
+}
+
+}
