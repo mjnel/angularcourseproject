51cd29626a531bff582684098e93c50e994d5dbd
Mark Nelson <mnel0001@MacBook-Pro.local>
2019-09-12 07:15:03 +0100

    WIP module

---
+++ src/app/recipes/recipe.module.ts
@@ -0,0 +1,23 @@
+
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+    declarations : [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent
+    ]
+})
+
+
+
+export class RecipeModule {
+
+
+
+
+}
