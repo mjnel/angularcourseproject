a2b06d18acc8ab2420aacb9183c01b44a6c0662a
Mark Nelson <mnel0001@MacBook-Pro.local>
2019-09-16 13:31:32 +0100

    upto date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipeModule } from './recipes/recipe.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
